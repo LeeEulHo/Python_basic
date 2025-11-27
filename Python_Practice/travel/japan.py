1ca674dffdf063ce682b627d96a942f20188c9ec
Lee Eul Ho <nuclearleh1207@naver.com>
2025-11-28 00:30:37 +0900

    update 2025/11/28

---
+++ Python_Practice/travel/japan.py
@@ -0,0 +1,3 @@
+class JapanPackage:
+    def detail(self):
+        print("[일본 패키지 3박 5일] 방콕, 파타야 여행 (야시장 투어) 50만원")
---
+++ Python_Practice/travel/usa.py
@@ -0,0 +1,3 @@
+class UsaPackage:
+    def detail(self):
+        print("[미국 패키지 3박 5일] LA 관광 여행 400만원")
